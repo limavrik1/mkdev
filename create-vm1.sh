3663733fbdfda132cf1a81a43e5d16994ce815e8
limavrik1 <limavrik@gmail.com>
2018-02-26 10:56:47 -0800

    Normalize all the line endings

--- create-vm1.sh
+++
@@ -1,6 +0,0 @@
-virt-install --name mkdev-vm-1 \
---location ./extra/CentOS-7-x86_64-Minimal-1708.iso \
---memory=768 --vcpus=1 --disk size=8 --cpu host \
---initrd-inject ./extra/ks.cfg \
---nographics \
---extra-args="ks=file:/ks.cfg console=tty0 console=ttyS0,115200n8"
